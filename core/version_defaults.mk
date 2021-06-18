Havoc-OS/android_build

build: Add support for faking platform security patch level
SafetyNet will fail if the fake build fingerprints don't match the
platform security patch level, which is a problem for devices that are
no longer supported by the manufacturer.

To fix this issue, allow legacy products to define a fake security patch
level from their last official update and set the standard patch level
property to that. The real patch level is moved to another custom
property that is shown to the user, e.g. in Settings -> About.

Note that this requires the "core: Use real security patch level
property" commit in frameworks/base for users to see the correct patch
level in Settings.

Change-Id: I296185a54fe1f7da9faaa13eef35f3db4231daf0

core/version_defaults.mk
@@ -242,7 +242,7 @@ ifndef PLATFORM_SECURITY_PATCH
     #  If there is no $PLATFORM_SECURITY_PATCH set, keep it empty.
       PLATFORM_SECURITY_PATCH := 2021-05-05
 endif
-.KATI_READONLY := PLATFORM_SECURITY_PATCH
+PLATFORM_REAL_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 ifndef PLATFORM_SECURITY_PATCH_TIMESTAMP
   # Used to indicate the matching timestamp for the security patch string in PLATFORM_SECURITY_PATCH.

Night-Raids-Reborn/tenx-build_make

Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

core/version_defaults.mk
@@ -243,6 +243,7 @@ ifndef PLATFORM_SECURITY_PATCH
       PLATFORM_SECURITY_PATCH := 2021-06-05
 endif
 .KATI_READONLY := PLATFORM_SECURITY_PATCH
+PLATFORM_FAKE_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 ifndef PLATFORM_SECURITY_PATCH_TIMESTAMP
   # Used to indicate the matching timestamp for the security patch string in PLATFORM_SECURITY_PATCH.